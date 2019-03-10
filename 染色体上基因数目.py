3731ae5d9410fc161be2545d1c0677a7cc39fcff
zhaominqiong <48174264+zhaominqiong@users.noreply.github.com>
2019-03-10 09:19:47 +0800

    Add files via upload

---
+++ 染色体上基因数目.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+"""
+myfile=open('C:\\Users\\Administrator\\Desktop\\贺老师\\python_project','r')
+lines=myfile.readlines()
+wordCounts={}
+for line in lines:
+    list_word=line.split()
+    num=str(list_word[2])
+    if num in wordCounts:
+        wordCounts[num]+=1
+    else:
+        wordCounts[num]=1
+
+print(wordCounts）
+"""
+print(123)
